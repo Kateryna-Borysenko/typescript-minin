7067096aa080e2fa50b92740fec4f786234e5fd9
Kateryna Borysenko <81910238+Kateryna-Borysenko@users.noreply.github.com>
2022-10-04 14:36:02 +0300

    Update 3_enum.ts

--- 3_enum.ts
+++ 3_enum.ts
@@ -1,3 +1,5 @@
+//enum - Энум перечесление -> позволяет структурировать код если есть какие то перечесления, есть два вида записи
+//необычная реализация энумов чтобы получить значение нужно обратится к индексу 
 enum Membership {
   Simple,
   Standard,
@@ -10,6 +12,8 @@ const membershipReverse = Membership[2]
 console.log(membership)
 console.log(membershipReverse)
 
+//========
+//второй вариант записи более понятен
 enum SocialMedia {
   VK = 'VK',
   FACEBOOK = 'FACEBOOK',
