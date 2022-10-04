223bf841bbeba655fe60267702a2f302877e2a63
Kateryna Borysenko <81910238+Kateryna-Borysenko@users.noreply.github.com>
2022-10-04 15:00:00 +0300

    Update 7_generic.ts

--- 7_generic.ts
+++ 7_generic.ts
@@ -2,10 +2,9 @@ const arrayOfNumbers: Array<number> = [1, 1, 2, 3, 5]
 const arrayOfStrings: Array<string> = ['Hello', 'Vladilen']
 
 
-function reverse<T>(array: T[]): T[] {
+function reverse<T>(array: T[]): T[] { // function reverse(array) для указания ф-ции которая работает с разными типами данных T[] - массив типа Т и возвращать также может различные массивы
   return array.reverse()
 }
 
 reverse(arrayOfNumbers)
 reverse(arrayOfStrings)
-
