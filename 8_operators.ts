1225922be891deedc1b5cb82bf9c3e6204a9a913
Kateryna Borysenko <81910238+Kateryna-Borysenko@users.noreply.github.com>
2022-10-04 15:06:16 +0300

    Update 8_operators.ts

--- 8_operators.ts
+++ 8_operators.ts
@@ -3,10 +3,12 @@ interface Person {
   age: number
 }
 
-type PersonKeys = keyof Person // 'name' | 'age'
+type PersonKeys = keyof Person // 'name' | 'age' keyof -> позволяет брать ключи с интрфейса
 
 let key: PersonKeys = 'name'
 key = 'age'
+
+//==========
 
 type User = {
   _id: number
@@ -15,8 +17,8 @@ type User = {
   createdAt: Date
 }
 
-type UserKeysNoMeta1 = Exclude<keyof User, '_id' | 'createdAt'> // 'name' | 'email'
-type UserKeysNoMeta2 = Pick<User, 'name' | 'email'> // 'name' | 'email'
+type UserKeysNoMeta1 = Exclude<keyof User, '_id' | 'createdAt'> // 'name' | 'email' // Exclude - исключает
+type UserKeysNoMeta2 = Pick<User, 'name' | 'email'> // 'name' | 'email' //Pick - осотавляет нужные поля
 
 
 let u1: UserKeysNoMeta1 = 'name'
